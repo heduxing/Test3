4fe8c77a3b00c0c73e53d5d5aaf8ca69df8da5c6
heduxing <46128392+heduxing@users.noreply.github.com>
2019-09-29 17:46:56 +0800

    Add files via upload
    hello

---
+++ Const.java
@@ -0,0 +1,13 @@
+package com.sino.crmfile.common;
+
+/**
+ * Created By kane at 2018/7/4 下午4:53
+ * 本服务常量类
+ */
+public class Const {
+
+    public static final String FILE_PATH_GROUP_NAME = "FILE_PATH";
+    public static final String FILE_PATH_NAME = "FILE_UPLOAD_PATH";
+    public static final String APP_SUFFIX = "_app";
+
+}
